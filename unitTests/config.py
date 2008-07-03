e7034d5c3d77216222dd9ba73f41a8fc79d1677c
W.N.S. Butler <butler@comnets.rwth-aachen.de>
2008-07-03 14:35:26 +0200

    Removed module projname from unit tests

--- unitTests/config.py
+++ unitTests/config.py
@@ -1,8 +1,6 @@
 # import the openWNS module. Contains all sub-classes needed for
 # configuration of openWNS
 import openwns
-# this is a project which shows how to write a loadable module to extend openWNS
-import projname
 
 simulator = openwns.Simulator()
 
