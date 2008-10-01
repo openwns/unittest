2bb60c775dc2d41d1d69c7324d34aa817d3d159c
Daniel Bültmann <dbn@comnets.rwth-aachen.de>
2008-10-01 17:32:25 +0200

    Added openwns-constanze and openwns-simpletl to unit tests

--- unitTests/config.py
+++ unitTests/config.py
@@ -3,6 +3,10 @@
 import openwns
 # this is a project which shows how to write a loadable module to extend openWNS
 import projname
+# the load generator
+import constanze
+# simplified transport layer
+import simpleTL
 
 simulator = openwns.Simulator()
 
