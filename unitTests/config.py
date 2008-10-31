9639f80eb2cb66e70e47f605398a6b4ba1935e55
Maciej Muehleisen <mue@comnets.rwth-aachen.de>
2008-10-31 18:21:31 +0100

    Changed output strategy to delete, added output dir to ignore, added tests from modules

--- unitTests/config.py
+++ unitTests/config.py
@@ -1,9 +1,21 @@
 # import the openWNS module. Contains all sub-classes needed for
 # configuration of openWNS
+moduleList = ["constanze", "simpleTL"]
+
 import openwns
+
+for module in moduleList:
+    try:
+	__import__(module)
+    except ImportError:
+	print "C++ Unit Test Warning: Module " + module + " is not available\n"
+	
 
 simulator = openwns.Simulator()
 
 simulator.environment.masterLogger.enabled = False
+
+simulator.outputStrategy = openwns.simulator.OutputStrategy.DELETE
+simulator.masterLogger.enabled = False
 
 openwns.setSimulator(simulator)
